2c096c5afa6829b56c9819ee1d3e465367b0b155
Maksim Zhiganov <99797849+justasimplesound@users.noreply.github.com>
2023-01-01 19:52:05 +0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-TOKEN = "5673767467:AAFgmjudvJGJbEeRT8deQ1nFpafAYTW-BXI"
-tags = ['транспорт', 'механический', 'люди', 'природа', 'музыка', 'помещение', '\U0001F384 ' + 'New Year!']
-allowed_formats = ["mp3", "flac", "fla", "ogg", "oga", "aiff", "aif", "wav", "m4a", "caf", "weba", "wave", "aac"]
-YaToken = "y0_AgAAAABVr3ssAAi7kAAAAADVa3SrPrGAxpaORF-deNp9JM-tQJN86kM"
